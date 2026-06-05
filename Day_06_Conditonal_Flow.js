868f972fa89659d0411eb8131554d84032739e62
DasAnimesh88 <animesh.das00@gmail.com>
2026-06-05 20:00:02 +0530

    updated

---
+++ Day_06_Conditonal_Flow.js
@@ -0,0 +1,53 @@
+// let age = 20;
+// if (age >= 18) {
+//   console.log("Adult — can vote");      // runs
+// } else {
+//   console.log("Minor — cannot vote");
+// }
+
+
+// --- if / else if / else ---
+// let score = 75;
+// let grade;
+// if (score >= 90) {
+//   grade = "A";
+// } else if (score >= 80) {
+//   grade = "B";
+// } else if (score >= 70) {
+//   grade = "C";
+// } else if (score >= 60) {
+//   grade = "D";
+// } else {
+//   grade = "F";
+// }
+// console.log(`Score: ${score} → Grade: ${grade}`);   // Score: 75 → Grade: C
+
+// Iterate over array (classic way)
+// let fruits = ["apple", "banana", "cherry", "date"];
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(`${i}: ${fruits[i]}`);
+// }
+
+
+// let fruits = ["apple", "banana", "cherry", "date"];
+
+// for (let i = 0; i < fruits.length; i++) {
+//   console.log(`${i}: ${fruits[i]}`);
+// }
+// ========================================================================
+// let str="Function.executeUserEntryPoint [as runMain] (node:internal/modules/run_main:170:5)"
+// str.charAt(0) // 'F'
+// str.indexOf('170') // 35
+
+
+// Array — gets values directly
+// let colors = ["red", "green", "blue"];
+// for (let color of colors) {
+//   console.log(color);   // red, green, blue
+// }
+
+// Array — gets values directly
+let colors = "Red"
+for (let color of colors) {
+  console.log(color);   // red, green, blue
+}
