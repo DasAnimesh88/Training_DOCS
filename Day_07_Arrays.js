b0c1764f850dfa59c352a5d06e87432018a8b538
DasAnimesh88 <animesh.das00@gmail.com>
2026-06-10 20:07:20 +0530

    updated

---
+++ Day_07_Arrays.js
@@ -0,0 +1,24 @@
+// let number =[0,1,2,3,4,5,6,7,8,9];
+// console.log(number.splice(2,2,10,11,13))
+// console.log(number)
+
+// let x =["Jenna","Mia","Lily","Sophie","Emily"];
+// console.log(x.splice(2,2,"Alice","Bob"))
+// console.log(x)
+
+
+// const arr2 = [1, 2];
+// const arr3 = [3, 4];
+// const arr4 = [5, 6];
+
+// let finaArr=arr2.concat(arr3,arr4)
+// console.log(finaArr)
+// let finaArr2=arr2.concat(arr3,arr4,["hello","world"])
+// console.log(finaArr2)
+
+// const words = ["Hello", "World", "from", "JS"];
+// let v=words.join("");
+// console.log("join with space:", v);// Hello World from JS // HelloWorldfromJS
+// console.log(typeof v);//string
+
+console.log()
